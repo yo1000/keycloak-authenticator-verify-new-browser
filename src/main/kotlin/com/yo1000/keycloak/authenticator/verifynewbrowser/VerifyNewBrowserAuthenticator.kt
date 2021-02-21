095a4cfbb3fe3cccd2fcc9956385627154319607
yo1000 <i@yo1000.com>
2021-02-22 03:51:54 +0900

    Refactor authenticator class to integrate factory class

--- src/main/kotlin/com/yo1000/keycloak/authenticator/verifynewbrowser/VerifyNewBrowserAuthenticator.kt
+++ src/main/kotlin/com/yo1000/keycloak/authenticator/verifynewbrowser/VerifyNewBrowserAuthenticator.kt
@@ -3,28 +3,83 @@ package com.yo1000.keycloak.authenticator.verifynewbrowser
 import org.jboss.logging.Logger
 import org.jboss.resteasy.spi.HttpRequest
 import org.jboss.resteasy.spi.HttpResponse
-import org.keycloak.authentication.AuthenticationFlowContext
-import org.keycloak.authentication.Authenticator
-import org.keycloak.authentication.CredentialValidator
+import org.keycloak.Config
+import org.keycloak.authentication.*
 import org.keycloak.common.util.ServerCookie
 import org.keycloak.common.util.Time
 import org.keycloak.credential.CredentialProvider
 import org.keycloak.email.EmailTemplateProvider
-import org.keycloak.models.KeycloakSession
-import org.keycloak.models.RealmModel
-import org.keycloak.models.UserModel
+import org.keycloak.models.*
+import org.keycloak.provider.ProviderConfigProperty
 import java.net.URI
 import java.time.Duration
 import java.util.*
 import javax.ws.rs.core.Cookie
 import javax.ws.rs.core.HttpHeaders
 
-class VerifyNewBrowserAuthenticator : Authenticator, CredentialValidator<VerifyNewBrowserCredentialProvider> {
+class VerifyNewBrowserAuthenticator : Authenticator, CredentialValidator<VerifyNewBrowserCredentialProvider>,
+        AuthenticatorFactory, ConfigurableAuthenticatorFactory {
     companion object {
+        const val ID = "verify-new-browser-authenticator"
+        const val CONFIG_PROPS_COOKIE_MAX_AGE_BROWSER_ID = "cookie.maxAge.browserId"
+        const val CONFIG_PROPS_COOKIE_MAX_AGE_BROWSER_ID_DEFAULT = 60 * 60 * 24 * 365 * 10 // Default: 10 years
+        const val CONFIG_PROPS_JWT_MAX_AGE_AUTHN_CHALLENGE = "jwt.maxAge.authenticationChallenge"
+        const val CONFIG_PROPS_JWT_MAX_AGE_AUTHN_CHALLENGE_DEFAULT = 60 * 60 // Default: 1 hour
         const val COOKIE_NAME_BROWSER_ID = "VERIFY_NEW_BROWSER_ID"
+
+        val REQUIREMENT_CHOICES = arrayOf(
+                AuthenticationExecutionModel.Requirement.REQUIRED,
+                AuthenticationExecutionModel.Requirement.ALTERNATIVE,
+                AuthenticationExecutionModel.Requirement.DISABLED
+        )
+
+        val configProps: MutableList<ProviderConfigProperty> = mutableListOf(
+                ProviderConfigProperty().also {
+                    it.name = CONFIG_PROPS_COOKIE_MAX_AGE_BROWSER_ID
+                    it.label = "Browser identification cookie Max age"
+                    it.type = ProviderConfigProperty.STRING_TYPE
+                    it.helpText = "Max age in seconds of the Browser ID Cookie"
+                    it.defaultValue = CONFIG_PROPS_COOKIE_MAX_AGE_BROWSER_ID_DEFAULT
+                },
+                ProviderConfigProperty().also {
+                    it.name = CONFIG_PROPS_JWT_MAX_AGE_AUTHN_CHALLENGE
+                    it.label = "Authentication challenge JWT Max age"
+                    it.type = ProviderConfigProperty.STRING_TYPE
+                    it.helpText = "Max age in seconds of the Authentication challenge JWT"
+                    it.defaultValue = CONFIG_PROPS_JWT_MAX_AGE_AUTHN_CHALLENGE_DEFAULT
+                }
+        )
 
         val logger: Logger = Logger.getLogger(VerifyNewBrowserAuthenticator::class.java)
     }
+
+    /* Authenticator Metadata */
+
+    override fun getId(): String = ID
+
+    override fun getDisplayType(): String = "Verify New Browser"
+
+    override fun getReferenceCategory(): String = displayType
+
+    override fun getHelpText(): String = "An email with a token URL that must be confirmed when a user signs in from a new browser."
+
+    override fun getRequirementChoices(): Array<AuthenticationExecutionModel.Requirement> = REQUIREMENT_CHOICES
+
+    override fun isConfigurable(): Boolean = true
+
+    override fun getConfigProperties(): MutableList<ProviderConfigProperty> = configProps
+
+    override fun isUserSetupAllowed(): Boolean = false
+
+    override fun init(scope: Config.Scope?) {}
+
+    override fun postInit(factory: KeycloakSessionFactory?) {}
+
+    override fun create(session: KeycloakSession?): Authenticator = this
+
+    override fun close() {}
+
+    /* Authenticator */
 
     override fun authenticate(context: AuthenticationFlowContext) {
         val session: KeycloakSession = context.session
@@ -62,10 +117,10 @@ class VerifyNewBrowserAuthenticator : Authenticator, CredentialValidator<VerifyN
         val rawBrowserId: String = UUID.randomUUID().toString()
         val challengeTokenMaxAge: Int = context.authenticatorConfig
                 ?.config
-                ?.get(VerifyNewBrowserAuthenticatorFactory.CONFIG_PROPS_JWT_MAX_AGE_AUTHN_CHALLENGE)
+                ?.get(CONFIG_PROPS_JWT_MAX_AGE_AUTHN_CHALLENGE)
                 ?.toInt()
                 ?.takeIf { it > 0 }
-                ?: VerifyNewBrowserAuthenticatorFactory.CONFIG_PROPS_JWT_MAX_AGE_AUTHN_CHALLENGE_DEFAULT
+                ?: CONFIG_PROPS_JWT_MAX_AGE_AUTHN_CHALLENGE_DEFAULT
 
         val actionToken: VerifyNewBrowserActionToken = VerifyNewBrowserActionToken(
                 context.user.id, Time.currentTime() + challengeTokenMaxAge, rawBrowserId)
@@ -84,10 +139,10 @@ class VerifyNewBrowserAuthenticator : Authenticator, CredentialValidator<VerifyN
 
         val browserIdCookieMaxAge: Int = context.authenticatorConfig
                 ?.config
-                ?.get(VerifyNewBrowserAuthenticatorFactory.CONFIG_PROPS_COOKIE_MAX_AGE_BROWSER_ID)
+                ?.get(CONFIG_PROPS_COOKIE_MAX_AGE_BROWSER_ID)
                 ?.toInt()
                 ?.takeIf { it > 0 }
-                ?: VerifyNewBrowserAuthenticatorFactory.CONFIG_PROPS_COOKIE_MAX_AGE_BROWSER_ID_DEFAULT
+                ?: CONFIG_PROPS_COOKIE_MAX_AGE_BROWSER_ID_DEFAULT
 
         context.addCookie(COOKIE_NAME_BROWSER_ID, rawBrowserId, browserIdCookieMaxAge)
         context.challenge(context.form().createForm("verify-new-browser-challenge.ftl"))
@@ -102,10 +157,10 @@ class VerifyNewBrowserAuthenticator : Authenticator, CredentialValidator<VerifyN
         val rawBrowserId: String = UUID.randomUUID().toString()
         val challengeTokenMaxAge: Int = context.authenticatorConfig
                 ?.config
-                ?.get(VerifyNewBrowserAuthenticatorFactory.CONFIG_PROPS_JWT_MAX_AGE_AUTHN_CHALLENGE)
+                ?.get(CONFIG_PROPS_JWT_MAX_AGE_AUTHN_CHALLENGE)
                 ?.toInt()
                 ?.takeIf { it > 0 }
-                ?: VerifyNewBrowserAuthenticatorFactory.CONFIG_PROPS_JWT_MAX_AGE_AUTHN_CHALLENGE_DEFAULT
+                ?: CONFIG_PROPS_JWT_MAX_AGE_AUTHN_CHALLENGE_DEFAULT
 
         val actionToken: VerifyNewBrowserActionToken = VerifyNewBrowserActionToken(
                 context.user.id, Time.currentTime() + challengeTokenMaxAge, rawBrowserId)
@@ -131,10 +186,10 @@ class VerifyNewBrowserAuthenticator : Authenticator, CredentialValidator<VerifyN
 
         val browserIdCookieMaxAge: Int = context.authenticatorConfig
                 ?.config
-                ?.get(VerifyNewBrowserAuthenticatorFactory.CONFIG_PROPS_COOKIE_MAX_AGE_BROWSER_ID)
+                ?.get(CONFIG_PROPS_COOKIE_MAX_AGE_BROWSER_ID)
                 ?.toInt()
                 ?.takeIf { it > 0 }
-                ?: VerifyNewBrowserAuthenticatorFactory.CONFIG_PROPS_COOKIE_MAX_AGE_BROWSER_ID_DEFAULT
+                ?: CONFIG_PROPS_COOKIE_MAX_AGE_BROWSER_ID_DEFAULT
 
         context.addCookie(COOKIE_NAME_BROWSER_ID, rawBrowserId, browserIdCookieMaxAge)
         context.challenge(context.form().createForm("verify-new-browser-challenge.ftl"))
@@ -147,8 +202,6 @@ class VerifyNewBrowserAuthenticator : Authenticator, CredentialValidator<VerifyN
     override fun setRequiredActions(session: KeycloakSession, realm: RealmModel, user: UserModel) {
         //user.addRequiredAction(UserModel.EMAIL_VERIFIED)
     }
-
-    override fun close() {}
 
     /* Providers */
     override fun getCredentialProvider(session: KeycloakSession): VerifyNewBrowserCredentialProvider {
